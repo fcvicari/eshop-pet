2b7be44209ba7a357665d4f55465386b7ae0cd5e
Fernando Cezar Vicari <fcvicari@gmail.com>
2023-05-06 00:26:29 -0300

    Login form included. Adjusted the Input component to use the zod component and react-hook-form.

--- epetweb/app/login/page.tsx
+++ epetweb/app/login/page.tsx
@@ -1,14 +1,41 @@
 "use client"
-
 import { FiEye, FiEyeOff, FiMail } from 'react-icons/fi'
 
+import { FormProvider, useForm } from "react-hook-form"
 import { Button } from '../src/components/button';
 import Image from 'next/image';
 import { Input } from '../src/components/input';
 import Link from 'next/link';
 import Logo from '../../public/logo.png';
+import { useState } from 'react';
+import { Title } from '../src/components/title';
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+const loginFormSchema = z.object({
+  email: z.string()
+    .nonempty('E-mail é obrigatório')
+    .email('Informe um e-mail válido')
+    .toLowerCase(),
+  password: z.string()
+    .nonempty('Senha é obrigatório')
+    .min(6, 'Informe no mínimo 6 caracteres'),
+})
+
+type loginFormDate = z.infer<typeof loginFormSchema>;
 
 export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const methods = useForm<loginFormDate>({
+    resolver: zodResolver(loginFormSchema),
+  });
+
+  function loginUser({ email, password }: loginFormDate) {
+    console.log(email)
+    console.log(password)
+  }
+
   return (
     <div className="bg-[url('/background.png')] bg-cover bg-no-repeat flex h-[87vh] md:h-screen w-screen items-center justify-center">
       <div className="bg-[var(--blue-800)] flex flex-col md:flex-row p-5 m-4 rounded-lg w-[24rem] md:w-[55rem] h-[31.875rem] drop-shadow-md justify-center items-center">
@@ -18,36 +45,43 @@ export default function Login() {
             alt="Logotipo E-Shop"
           />
         </div>
-        <div className="flex flex-col m-2 gap-4 md:m-6 items-center justify-between py-8 px-5 md:w-[50%] rounded-lg h-[100%] w-[100%] md:h-[90%] bg-[var(--blue-100)]">
-          <h1 className='text-2xl font-bold text-[var(--blue-800)]'>Faça seu login</h1>
+        <div className="flex flex-col m-2 gap-4 md:m-6 items-center justify-between py-4 md:py-8 px-5 md:w-[50%] rounded-lg h-[100%] w-[100%] md:h-[90%] bg-[var(--blue-100)]">
+          <Title size='large'>Faça seu login</Title>
 
-          <Input
-            required
-            name="email"
-            inputSize="full"
-            label='E-Mail'
-            type="email"
-            error="mensagem de erro"
-            Icon={FiMail}
-          />
+          <FormProvider {...methods}>
+            <form onSubmit={methods.handleSubmit(loginUser)} className='flex flex-col gap-2'>
+              <Input
+                required
+                inputSize="full"
+                label='E-Mail'
+                type="email"
+                nameField="email"
+              >
+                <FiMail />
+              </Input>
 
-          <Input
-            inputSize="full"
-            name="password"
-            label='Senha'
-            type="text"
-            Icon={FiEye}
-          />
+              <Input
+                required
+                inputSize="full"
+                label='Senha'
+                type={showPassword ? "text" : "password"}
+                nameField="password"
+              >
+                {showPassword ? <FiEyeOff onClick={() => setShowPassword(!showPassword)} /> : <FiEye onClick={() => setShowPassword(!showPassword)} />}
+              </Input>
 
-          <Button
-            label="Entrar"
-            variant="primary"
-            size="full"
-          />
+              <Button
+                type='submit'
+                label="Entrar"
+                variant="primary"
+                size="full"
+              />
+            </form>
+          </FormProvider>
 
-          <Link href="./login">Esqueci minha senha</Link>
+          <Link className='text-xs text-[var(--blue-800)]' href="./login">Esqueci minha senha</Link>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
--- epetweb/app/src/components/input/index.tsx
+++ epetweb/app/src/components/input/index.tsx
@@ -1,14 +1,19 @@
-import { roboto } from '@/app/config/fonts';
+import { useFormContext } from 'react-hook-form';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
-  error?: string;
+  nameField: string;
   inputSize: 'xs' | 'sm' | 'base' | 'lg' | 'xl' | 'xxl' | 'full';
   children?: React.ReactNode;
 }
 
-export function Input({ label, name, required, readOnly, error, inputSize, children, ...props }: InputProps) {
+export function Input({ label, nameField, required, readOnly, inputSize, children, ...props }: InputProps) {
+  const { register, formState: { errors } } = useFormContext();
+
+  const error = errors[nameField]?.message?.toString();
+
   const defaultConfig = `py-1 ${readOnly ? 'bg-[var(--gray-100)]' : 'bg-white'}  ${error ? '' : 'focus-within:border-[var(--yellow-500)]'} flex focus:bg-yellow-200 flex-row h-8 items-center justify-center w-full border-b-2 gap-1 rounded-lg p-2`;
+
   const inputClass = {
     size: {
       xs: 'w-24',
@@ -23,14 +28,14 @@ export function Input({ label, name, required, readOnly, error, inputSize, child
 
   return (
     <div className={`h-16 flex flex-col ${readOnly ? 'text-[var(--blue-300)]' : 'text-[var(--blue-800)]'} ${inputClass.size[inputSize]} `}>
-      <label htmlFor={name} className='flex gap-1 px-1 items-center text-xs w-72 text-[var(--blue-800)]'>
+      <label htmlFor={nameField} className='flex gap-1 px-1 items-center text-xs w-72 text-[var(--blue-800)]'>
         {label}
       </label>
       <div className={`${defaultConfig} ${error ? 'border-[var(--red-900)]' : 'border-[var(--blue-800)]'} ${required ? 'border-l-[6px]' : ''} `} >
         <input
-          name={name}
-          className={`${roboto.className} text-sm w-full bg-transparent outline-none`}
+          className="text-sm w-full bg-transparent outline-none"
           {...props}
+          {...register(nameField)}
         />
         <label className={`${readOnly ? '' : error ? 'text-[var(--red-900)]' : 'text-[var(--blue-800)]'}`} >
           {children}
