17b738766ebd0b37d78a80d3eec5ec9136137527
Fernando Cezar Vicari <fcvicari@gmail.com>
2023-05-05 21:36:15 -0300

    Added preview of when focus is on component and when component is required.

--- epetweb/app/src/components/input/index.tsx
+++ epetweb/app/src/components/input/index.tsx
@@ -1,20 +1,15 @@
-import { useCallback, useEffect, useState } from 'react';
-
-import { FiTarget } from 'react-icons/fi'
-import { IconBaseProps } from 'react-icons';
 import { roboto } from '@/app/config/fonts';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  required?: boolean;
   label: string;
   error?: string;
   inputSize: 'xs' | 'sm' | 'base' | 'lg' | 'xl' | 'xxl' | 'full';
-  Icon?: React.ComponentType<IconBaseProps>;
+  children?: React.ReactNode;
 }
 
-export function Input({ label, name, required, error, inputSize, Icon, ...props }: InputProps) {
+export function Input({ label, name, required, readOnly, error, inputSize, children, ...props }: InputProps) {
+  const defaultConfig = `py-1 ${readOnly ? 'bg-[var(--gray-100)]' : 'bg-white'}  ${error ? '' : 'focus-within:border-[var(--yellow-500)]'} flex focus:bg-yellow-200 flex-row h-8 items-center justify-center w-full border-b-2 gap-1 rounded-lg p-2`;
   const inputClass = {
-    default: 'flex flex-row h-8 items-center w-full bg-white justify-center gap-1 px-2 rounded-lg',
     size: {
       xs: 'w-24',
       sm: 'w-36',
@@ -24,66 +19,24 @@ export function Input({ label, name, required, error, inputSize, Icon, ...props 
       xxl: 'w-72',
       full: 'w-full',
     },
-    typeBorder: {
-      onFocus: 'border-[1px]',
-      onBlur: 'border-b-[1px] border-r-[1px]',
-    },
-    color: {
-      onFocus: 'var(--yellow-500)',
-      onBlur: 'var(--blue-600)',
-      error: 'var(--red-900)',
-    }
   };
-  
-  
-  const [typeBorder, setTypeBorder] = useState<'onFocus' | 'onBlur'>('onBlur');
-  const [cssConfig, setCssConfig] = useState<'onFocus' | 'onBlur' | 'error'>('onBlur')
-
-  const handleInputFocus = useCallback(() => {
-    setTypeBorder('onFocus');
-    if (!error) {
-      setCssConfig('onFocus');
-    }
-  }, []);
-
-  const handleInputBlur = useCallback(() => {
-    setTypeBorder('onBlur');
-    if (!error) {
-      setCssConfig('onBlur');
-    }
-  }, []);
-
-  useEffect(() => {
-    if (!error) {
-      setCssConfig('onBlur');
-    }
-    else {
-      setCssConfig('error');
-    }
-  }, [error]);
 
   return (
-    <div className={`flex flex-col text-[var(--blue-800)] ${inputClass.size[inputSize]} `}>
-      <label
-        htmlFor={name}
-        className='flex gap-1 items-center text-xs w-72 px-2'
-      >
+    <div className={`h-16 flex flex-col ${readOnly ? 'text-[var(--blue-300)]' : 'text-[var(--blue-800)]'} ${inputClass.size[inputSize]} `}>
+      <label htmlFor={name} className='flex gap-1 px-1 items-center text-xs w-72 text-[var(--blue-800)]'>
         {label}
-        {required && <FiTarget size={9} />}
       </label>
-      <div className={`${inputClass.default} border-[${inputClass.color[cssConfig]}] ${inputClass.typeBorder[typeBorder]} `} >
+      <div className={`${defaultConfig} ${error ? 'border-[var(--red-900)]' : 'border-[var(--blue-800)]'} ${required ? 'border-l-[6px]' : ''} `} >
         <input
-          onFocus={handleInputFocus}
-          onBlur={handleInputBlur}
           name={name}
           className={`${roboto.className} text-sm w-full bg-transparent outline-none`}
           {...props}
         />
-        {Icon && <Icon className={`text-[${inputClass.color[cssConfig]}]`} />}
+        <label className={`${readOnly ? '' : error ? 'text-[var(--red-900)]' : 'text-[var(--blue-800)]'}`} >
+          {children}
+        </label>
       </div>
-      <div className={`h-4 pl-2 text-xs w-full text-[${inputClass.color[cssConfig]}] `}>
-        {error && (<span>{error}</span>)}
-      </div>
+      {error && (<span className='h-4 pl-2 text-xs w-full text-[var(--red-900)]'>{error}</span>)}
     </div >
   );
 }
